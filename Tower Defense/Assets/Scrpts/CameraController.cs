fde9b4fe05e7470dd2e49fab10dd55a244cd6018
Trisardis <sacoop267@gmail.com>
2023-04-07 12:06:55 -0400

    Started shop UI
    Started creating the shop UI for towers

--- Tower Defense/Assets/Scrpts/CameraController.cs
+++ Tower Defense/Assets/Scrpts/CameraController.cs
@@ -2,20 +2,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    
+    // Camera movement variable initialization
     private bool movementAllowed = true;
-
     public float panSpeed = 30f;
     public float panBorderThickness = 10f;
+    public float scrollSpeed = 5f;
+    public float minimumY = 10f;
+    public float maximumY = 80f;
 
     // Update is called once per frame
     void Update()
     {
+        // Locks screen movement if escape is pressed
         if (Input.GetKeyDown(KeyCode.Escape))
             movementAllowed = !movementAllowed;
-
         if (!movementAllowed)
             return;
 
+        // Camera moves in the a given direction based on key or mouse input
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
             transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
@@ -33,6 +38,14 @@ public class CameraController : MonoBehaviour
             transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
         }
 
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 pos = transform.position;
+
+        pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minimumY, maximumY);
+        transform.position = pos;
     }
 }
 
