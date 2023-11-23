6baef7fd47b61f7f94d15f9847154fb057fdf2b8
Trisardis <sacoop267@gmail.com>
2023-11-23 14:22:40 -0500

    Added a Win Screen
    Added a win screen that displays when the player has survived all of the levels.

--- Tower Defense/Assets/Scrpts/GameManager.cs
+++ Tower Defense/Assets/Scrpts/GameManager.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static bool GameIsOver;
+    public static bool LevelCompleated;
 
 	public GameObject gameOverUI;
-	public GameObject completeLevelUI;
+	public GameObject LevelCompleatedUI;
 
 	void Start ()
 	{
+        LevelCompleated = false;
 		GameIsOver = false;
+        LevelCompleatedUI.SetActive(false);
         gameOverUI.SetActive(false);
 	}
 
     void Update () 
     {
+        Debug.Log(WaveSpawner.curretnWave);
         if (GameIsOver)
             return;
 
@@ -23,6 +27,16 @@ public class GameManager : MonoBehaviour
         {
             EndGame();
         }
+        if (WaveSpawner.curretnWave == 9 && WaveSpawner.EnemiesAlive == 0)
+        {
+            LevelWon();
+        }
+    }
+
+    void LevelWon ()
+    {
+        LevelCompleated = true;
+        LevelCompleatedUI.SetActive(true);
     }
 
     void EndGame ()
